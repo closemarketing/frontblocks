5a2dd677090a4988ed1a58fdc8195df82deac87d
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2024-04-04 13:21:29 +0200

    Update plugin version to 0.2.3

--- frontblocks.php
+++ frontblocks.php
@@ -3,7 +3,7 @@
  * Plugin Name: FrontBlocks
  * Plugin URI:  https://github.com/closemarketing/frontblocks
  * Description: Blocks and helpers to facilitate GeneratePress frontend developing.
- * Version:     0.2.2
+ * Version:     0.2.3
  * Author:      Closemarketing
  * Author URI:  https://close.marketing
  * Text Domain: frontblocks
@@ -27,7 +27,7 @@
 
 defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 
-define( 'FRBL_VERSION', '0.2.2' );
+define( 'FRBL_VERSION', '0.2.3' );
 define( 'FRBL_PLUGIN', __FILE__ );
 define( 'FRBL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'FRBL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
