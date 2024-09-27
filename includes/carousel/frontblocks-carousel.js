9bbbe5cd11997de74fec3b6a40fbc3588f707070
davidperezgar <david@closemarketing.es>
2024-09-27 11:49:30 +0200

    added colors

--- includes/carousel/frontblocks-carousel.js
+++ includes/carousel/frontblocks-carousel.js
@@ -28,6 +28,10 @@ window.addEventListener('load', function (event) {
 			carouselAutoplay = item.getAttribute('data-autoplay') ? item.getAttribute('data-autoplay') : 0;
 			carouselResView = item.getAttribute('data-res-view') ? item.getAttribute('data-res-view') : 1;
 
+			carouselbuttonsColor = item.getAttribute('data-buttons-color') ? item.getAttribute('data-buttons-color') : 'black';
+			carouselbuttonsBackgroundColor = item.getAttribute('data-buttons-background-color') ? item.getAttribute('data-buttons-background-color') : 'transparent';
+
+
 			// Add classes
 			item.classList.add( 'glide__slides', 'glide-' + Math.floor(Math.random() * 1000) );
 			for (const child of item.children) {
@@ -43,6 +47,7 @@ window.addEventListener('load', function (event) {
 					const bullet = document.createElement('button');
 					bullet.classList.add('glide__bullet');
 					bullet.setAttribute('data-glide-dir', '=' + i);
+					bullet.style.backgroundColor = carouselbuttonsBackgroundColor;
 					bullets.appendChild(bullet);
 				}
 
@@ -53,7 +58,17 @@ window.addEventListener('load', function (event) {
 				const arrows = document.createElement('div');
 				arrows.classList.add('glide__arrows');
 				arrows.setAttribute('data-glide-el', 'controls');
-				arrows.innerHTML = '<button class="glide__arrow glide__arrow--left glide__arrow glide__arrow--left" data-glide-dir="<"><svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 1L1 6L6 11" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button><button class="glide__arrow glide__arrow--right glide__arrow glide__arrow--right" data-glide-dir=">"><svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 11L6 6L1 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>';
+				arrowsHTML = '<button class="glide__arrow glide__arrow--left glide__arrow glide__arrow--left" data-glide-dir="<"';
+				arrowsHTML += ' style="background-color: ' + carouselbuttonsBackgroundColor + '"';
+				arrowsHTML += '><svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 1L1 6L6 11" stroke="';
+				arrowsHTML += carouselbuttonsColor;
+				arrowsHTML += '" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button><button class="glide__arrow glide__arrow--right glide__arrow glide__arrow--right" data-glide-dir=">"';
+				
+				arrowsHTML += ' style="background-color: ' + carouselbuttonsBackgroundColor + '"';
+				arrowsHTML += '><svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 11L6 6L1 1" stroke="';
+				arrowsHTML += carouselbuttonsColor;
+				arrowsHTML += '" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>';
+				arrows.innerHTML = arrowsHTML;
 				wrapperParent.appendChild(arrows);
 			}
 
