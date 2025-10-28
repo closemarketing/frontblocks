61dc2898627bf0974a6d44c5f5025410a618f876
castellon <alexcastellon077@gmail.com>
2025-10-28 08:45:29 +0100

    Translation added

--- includes/Frontend/Gallery.php
+++ includes/Frontend/Gallery.php
@@ -90,6 +90,13 @@ class Gallery {
 			FRBL_VERSION,
 			true
 		);
+
+		// Set script translations for JavaScript.
+		wp_set_script_translations(
+			'frontblocks-gallery-option',
+			'frontblocks'
+		);
+
 		// Enqueue gallery styles in editor for live preview.
 		wp_enqueue_style(
 			'frontblocks-gallery',
