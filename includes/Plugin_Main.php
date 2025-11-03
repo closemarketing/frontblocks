dd4f9319fcd84abfb5a0b2fdbd5b3e54d1f62332
castellon <alexcastellon077@gmail.com>
2025-11-03 13:15:40 +0100

    Reading time added

--- includes/Plugin_Main.php
+++ includes/Plugin_Main.php
@@ -95,6 +95,9 @@ class Plugin_Main {
 
 		// Counter module (GenerateBlocks Headline counter effect).
 		new Frontend\Counter();
+
+		// Reading Time module.
+		new Frontend\ReadingTime();
 	}
 
 	/**
