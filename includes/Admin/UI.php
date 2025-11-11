fe466dc2d7284802712e5a3773bb5bd2e87e9cb3
castellon <alexcastellon077@gmail.com>
2025-11-11 14:34:55 +0100

    Delete green icon

--- includes/Admin/UI.php
+++ includes/Admin/UI.php
@@ -28,12 +28,6 @@ class UI {
 		$icon = self::get_feature_icon( $feature_id );
 		?>
 		<div class="frbl-feature-card frbl-feature-active">
-			<div class="frbl-active-badge">
-				<svg class="tw-w-4 tw-h-4" fill="currentColor" viewBox="0 0 20 20">
-					<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-				</svg>
-			</div>
-			
 			<div class="frbl-feature-content">
 				<div class="frbl-feature-icon">
 					<?php echo $icon; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
